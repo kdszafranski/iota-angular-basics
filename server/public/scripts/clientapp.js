18745a5db6e8db0bf91fdca75ccc25bfe19e7715
Kris Szafranski <kdszafranski@gmail.com>
2016-08-22 15:22:09 -0500

    repeat, if, show

--- server/public/scripts/clientapp.js
+++ server/public/scripts/clientapp.js
@@ -2,8 +2,26 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('IndexController', ['$scope', function($scope) {
   console.log("hey, i'm working!");
+  $scope.people = [
+    {
+      firstname: 'Kris',
+      lastname: 'Szafranski'
+    },
+    {
+      firstname: 'Casie',
+      lastname: 'Siekman'
+    }
+  ];
 
-  $scope.firstname = 'Kris';
-  $scope.lastname = 'Szafranski';
+  $scope.addPerson = function() {
+    $scope.people.push({
+      firstname: 'Danny',
+      lastname: 'Ritter'
+    });
+  }
+
+  $scope.increment = function(number) {
+    $scope.total += number;
+  }
 
 }]);
